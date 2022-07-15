11875b97b2652117785e87099d576c4e0d1a6c35
Denis Es <denis.es@gs-wn.de>
2022-07-15 11:48:09 +0200

    Test

---
+++ Test.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * Beschreibung
+ *
+ * @version 1.0 vom 15.07.2022
+ * @author 
+ */
+
+public class Test {
+  
+  public static void main(String[] args) {
+    
+  } // end of main
+
+} // end of class Test
